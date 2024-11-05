d068342b6001bea0cca4d30f68864694f30fca97
matthewgstillman <matthewgstillman@gmail.com>
2024-11-05 16:09:25 -0700

    style updates, create new expense add to expenses page

--- obj/Debug/net8.0/ExpenseTracker.AssemblyInfo.cs
+++ obj/Debug/net8.0/ExpenseTracker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExpenseTracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b0b8bb3e0357dc11afa20273fbe712df4e1b10a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6f46639783202adc6b64baaf56e0116be0bed47")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExpenseTracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExpenseTracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
